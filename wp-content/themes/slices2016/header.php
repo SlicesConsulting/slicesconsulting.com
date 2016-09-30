0aa9532b6354023b58313973a14b9873c0f8dc2b
Julia Mayhugh <julia.mayhugh@gmail.com>
2016-09-30 11:57:53 -0600

    Add Google tag manager

--- wp-content/themes/slices2016/header.php
+++ wp-content/themes/slices2016/header.php
@@ -12,11 +12,19 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WKHQVN');</script>
+	<!-- End Google Tag Manager -->
+
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
