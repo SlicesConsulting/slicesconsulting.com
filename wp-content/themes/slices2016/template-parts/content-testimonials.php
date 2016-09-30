223ab2ab6c2666b344e5d12d56f917cbd810f607
Julia Mayhugh <julia.mayhugh@gmail.com>
2016-09-29 22:13:58 -0600

    Add testimonial styles

--- wp-content/themes/slices2016/template-parts/content-testimonials.php
+++ wp-content/themes/slices2016/template-parts/content-testimonials.php
@@ -14,40 +14,34 @@ while ( $cpt->have_posts() ) : $cpt->the_post();
 
 <div class="col-md-6 p1">
 	<div class="testimonial-wrap">
-		<div class="row">
-			<div class="col-md-2">
-					<img src="<?php the_field('profile'); ?>" alt=""  class="testimonial-img alignleft"/>								
-			</div>
-			<div class="col-md-10">
-					<?php
-					if(get_field('company'))
-					{
-						echo '<h3 class="textleft hidden-xs m0">' . get_field('company') . '</h3>';
-					} ;?>
-				<p><?php the_excerpt();?></p>
-				<div class="row">
-					<div class="col-xs-12 testimonial-content">
-					<h5>
-						<?php
-						if(get_field('name'))
-						{
-							echo get_field('name');
-						}
-						if(get_field('position'))
-						{
-							echo ', ' . get_field('position');
-						}
-						;?>
-					</h5>
-						<?php
-						if (has_post_thumbnail()) {
-							the_post_thumbnail();
-						};?>
-					</div>
-				</div>
-			</div>
+					
+		<div class="quote">
+			<?php the_excerpt();?>
 		</div>
-	</div>	
+
+		<img src="<?php the_field('profile'); ?>" alt=""  class="testimonial-img"/>								
+
+		<div class="testimonial-source">
+		<h5>
+			<?php
+			if(get_field('name'))
+			{
+				echo get_field('name');
+			}
+			if(get_field('position'))
+			{
+				echo ', ' . get_field('position');
+			}
+			;?>
+		</h5>
+
+		<?php
+		if (has_post_thumbnail()) {
+			the_post_thumbnail();
+		};?>
+		</div>
+
+	</div>
 </div>
 
 	<?php
