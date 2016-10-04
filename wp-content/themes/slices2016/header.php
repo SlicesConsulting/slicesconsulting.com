ccc56836552dddf0eb047f5b5bc44c2dc12356ad
Julia Mayhugh <julia.mayhugh@gmail.com>
2016-10-03 19:38:50 -0600

    Combine png favicon files to a single ico

--- wp-content/themes/slices2016/header.php
+++ wp-content/themes/slices2016/header.php
@@ -12,8 +12,10 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/favicon.ico" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<!-- Google Tag Manager -->
